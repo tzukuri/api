14548aca690cbb58b3681a28029da4dce129b399
Will Cannings <me@willcannings.com>
2014-09-30 00:30:42 +1000

    Share buttons

--- app/assets/javascripts/share.js
+++ app/assets/javascripts/share.js
@@ -1,9 +1,12 @@
 $(function() {
+    var url = null;
+
     $('a.share').click(function(event) {
         var panel = $(this).closest('article');
+        $('article.blur').removeClass('blur');
         panel.addClass('blur');
 
-        var url = window.location.href.split('#')[0] + '#' + panel[0].id;
+        url = window.location.href.split('#')[0] + '#' + panel[0].id;
 
         $('#share-panel').css('top', panel.position().top + 60);
         $('#share-panel').css('height', panel.height());
@@ -15,6 +18,30 @@ $(function() {
     $('#close-share-panel').click(function(event) {
         $('article.blur').removeClass('blur');
         $('#share-panel').fadeOut();
+        event.preventDefault();
+    });
+
+    $('#share-fb').click(function(event) {
+        event.preventDefault();
+    });
+
+    $('#share-twitter').click(function(event) {
+        event.preventDefault();
+    });
+    
+    $('#share-pinterest').click(function(event) {
+        event.preventDefault();
+    });
+    
+    $('#share-tumblr').click(function(event) {
+        event.preventDefault();
+    });
+    
+    $('#share-email').click(function(event) {
+        event.preventDefault();
+    });
+    
+    $('#share-link').click(function(event) {
         event.preventDefault();
     });
 });
