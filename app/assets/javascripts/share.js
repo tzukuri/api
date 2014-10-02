5ef56952f40bb696a1ab94732c420c321024f16a
Allen <a@tzukuri.com>
2014-10-02 20:47:26 +1000

    blah

--- app/assets/javascripts/share.js
+++ app/assets/javascripts/share.js
@@ -1,14 +1,27 @@
 $(function() {
     var url = null;
+    var pairs = false;
 
     $('a.share').click(function(event) {
         var panel = $(this).closest('article');
-        $('article.blur').removeClass('blur');
+
+        if (panel[0].id == 'whichpair') {
+          var panel = $(this).closest('section');
+          $('section.blur').removeClass('blur');
+          pairs = true
+        } else {
+          $('article.blur').removeClass('blur');
+          pairs = false;
+        }
+
         panel.addClass('blur');
 
         url = window.location.href.split('#')[0] + '#' + panel[0].id;
 
-        $('#share-panel').css('top', panel.position().top + 60);
+        if (pairs)
+          $('#share-panel').css('top', panel.position().top + 360);
+        else
+          $('#share-panel').css('top', panel.position().top + 60);
         $('#share-panel').css('height', panel.height());
         $('#share-panel').fadeIn();
 
@@ -17,6 +30,7 @@ $(function() {
 
     $('#close-share-panel').click(function(event) {
         $('article.blur').removeClass('blur');
+        $('section.blur').removeClass('blur');
         $('#share-panel').fadeOut();
         event.preventDefault();
     });
@@ -28,19 +42,19 @@ $(function() {
     $('#share-twitter').click(function(event) {
         event.preventDefault();
     });
-    
+
     $('#share-pinterest').click(function(event) {
         event.preventDefault();
     });
-    
+
     $('#share-tumblr').click(function(event) {
         event.preventDefault();
     });
-    
+
     $('#share-email').click(function(event) {
         event.preventDefault();
     });
-    
+
     $('#share-link').click(function(event) {
         event.preventDefault();
     });
