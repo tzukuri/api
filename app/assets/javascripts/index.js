948a34d50fab123bd31c6b4ca7fdd1e22f3c9023
Will Cannings <me@willcannings.com>
2014-10-06 17:10:17 +1100

    environment, ibeacon and countdown panels

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -15,4 +15,17 @@ $(function() {
         option.addClass('selected');
         event.preventDefault();
     });
+
+    countdown(
+        // number of milliseconds since Unix epoch UTC. required since new Date() returns local time.
+        // js date measures months from 0, so 8 == September. hours adjusted for EST summer time (UTC -4)
+        Date.UTC(2014, 8, 19, 4),
+        function(ts) {
+            $('#countdown-days').text(ts.days);
+            $('#countdown-hours').text(ts.hours);
+            $('#countdown-minutes').text(ts.minutes);
+            $('#countdown-seconds').text(ts.seconds);
+        },
+        countdown.DEFAULTS // trigger callback roughly once per second
+    );
 });
