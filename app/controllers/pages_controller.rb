14d5fadf105066898e78d0af17ae92dd18dd855e
Will Cannings <me@willcannings.com>
2014-10-13 13:57:03 +1100

    Page titles and demographic tracking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
     def index
         @html_klass = params[:page]
+        @page_title = params[:page].titleize unless params[:page] == 'index'
         render action: params[:page]
     end
 end
