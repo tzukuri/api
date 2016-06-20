4223558628b6a5cd2f74c14a176c9d770e942369
Sam Turner <sam@tzukuri.com>
2016-06-20 16:00:34 +1000

    Updated mail templates to work on Outlook/Gmail

--- app/mailers/beta_mailer.rb
+++ app/mailers/beta_mailer.rb
@@ -3,7 +3,7 @@ class BetaMailer < ActionMailer::Base
 
   def send_beta_confirmation_email(beta_user)
     @beta_user = beta_user
-    mail(to: beta_user.email, subject:"Thanks for registering your interest in the Tzukuri Beta")
+    mail(to: beta_user.email, subject:"Thanks for registering for the Tzukuri Beta")
   end
 
   def send_beta_acceptance_email(beta_user)
@@ -14,7 +14,7 @@ class BetaMailer < ActionMailer::Base
   def send_beta_forgot_link(beta_user)
     puts beta_user.email
     @beta_user = beta_user
-    mail(to: beta_user.email, subject: "Your Tzukuri Beta login link")
+    mail(to: beta_user.email, subject: "Your Tzukuri Beta dashboard link")
   end
 
 end
