a8706c942f02d64e4a8e80f898c7668d44a45ee1
Sam Turner <sam@tzukuri.com>
2016-06-22 17:29:24 +1000

    Removed unused JS

--- app/assets/javascripts/beta/forgot.js
+++ app/assets/javascripts/beta/forgot.js
@@ -10,10 +10,8 @@ $(function() {
 
     // creating a new response to a survey question
     $('#beta_user_retrieve').on('ajax:success', function(e, data) {
+        console.log(data)
         if (data.success) {
-
-            $("#sent-email").html(data.email)
-
             $("#submit-btn").fadeOut(function() {
                 $("#email-confirm").html('If a matching account was found an email was sent with your login link').fadeIn();
                 $("#email").prop('disabled', true);
@@ -21,6 +19,7 @@ $(function() {
         }
     }).on('ajax:error', function(e, data) {
         // todo: handle the error state
+        console.log(data)
     });
 
     $('#beta_user_retrieve').on('submit', function(e) {
