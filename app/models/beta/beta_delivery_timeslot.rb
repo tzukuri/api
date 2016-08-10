7b0998aa5dee53a679e2e3aae8f2d9e6f7d19048
Sam Turner <samturner.23@me.com>
2016-08-10 12:08:15 +1000

    NSW is now the default state. Updated end time

--- app/models/beta/beta_delivery_timeslot.rb
+++ app/models/beta/beta_delivery_timeslot.rb
@@ -5,7 +5,7 @@ class BetaDeliveryTimeslot < ActiveRecord::Base
 
   # return a hash that maps each day -> all the available timeslots for that day
   def self.all_available_timeslots
-    end_date = Date.parse("12/08/2016")
+    end_date = Date.parse("31/12/2016")
     tomorrow = Date.tomorrow
     num_days = (end_date - tomorrow).to_i
 
