56a9e990c4ed8d66c7f229b8a2b5714144c8c8ba
Sam Turner <samturner.23@me.com>
2016-08-15 17:24:24 +1000

    Writing images to the correct folder

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,21 +1,21 @@
 class Feedback < ActiveRecord::Base
   belongs_to :user
-
-  before_validation :decode_attachment_data
+  has_attached_file :attachment, :path => ":rails_root/feedback/:id/:basename.:extension"
 
   attr_accessor :attachment_data
-  has_attached_file :attachment
 
+  before_validation :decode_attachment_data
   validates :topic, presence: true
   validates :content, presence: true
 
+  # images should be smaller than 10mb and an image content type
   validates_attachment :attachment, size: {in: 0..10.megabytes},
                             content_type: {content_type: /^image\/(jpeg|png|gif|tiff)$/}
 
   private
 
   def decode_attachment_data
-
+    # if we have attachment data, decode the image from base64
     if attachment_data.present?
       data = StringIO.new(Base64.decode64(attachment_data))
 
@@ -26,5 +26,4 @@ class Feedback < ActiveRecord::Base
       self.attachment = data
     end
   end
-
 end
