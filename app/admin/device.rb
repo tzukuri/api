13effe504bcfcddf92da2ff5428ee0c96b3dc575
Sam Turner <samturner.23@me.com>
2016-08-16 13:09:36 +1000

    Added more details to device

--- app/admin/device.rb
+++ app/admin/device.rb
@@ -1,19 +1,23 @@
 ActiveAdmin.register Device do
-    permit_params :mac_address, :frame, :size, :colour, :frame_manufacture_ts,
+    permit_params :mac_address, :design, :size, :colour, :frame_manufacture_ts,
                   :board_manufacture_ts, :board_revision, :frame_revision,
-                  :firmware_version, :charge_qc_pass_ts, :rf_qc_pass_ts, :shipped
+                  :firmware_version, :charge_qc_pass_ts, :rf_qc_pass_ts, :shipped, :optical
 
     index do
         selectable_column
         id_column
         column :mac_address
-        column :frame
+        column :design
         column :size
         column :colour
+        column :hardware_revision
+        column :serial
+        column :pin
+        column :optical
         actions
     end
 
-    filter :mac_address
-    filter :frame
-    filter :shipped
+    # filter :mac_address
+    # filter :frame
+    # filter :shipped
 end
