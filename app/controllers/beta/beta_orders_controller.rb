afb1b6a36437a747b52ee3230d60ec0c77b55b39
Sam Turner <samturner.23@me.com>
2016-08-26 16:55:57 +1000

    Cleaned up BetaOrder controller and moved booking logic to BetaOrder to allow for updating orders independently

--- app/controllers/beta/beta_orders_controller.rb
+++ app/controllers/beta/beta_orders_controller.rb
@@ -3,67 +3,26 @@ class BetaOrdersController < ApplicationController
   http_basic_authenticate_with name: "beta@tzukuri.com", password: "ksV-Pxq-646-feS", only: [:all, :date]
 
   def create
-    begin
-      # if the user is getting personal delivery create an order with delivery timeslot
-      if params[:timeslot_start].present? && params[:timeslot_end].present?
-        # create an order with timeslot_start
-        start_time = params[:timeslot_start]
-        end_time = params[:timeslot_end]
+    beta_order = BetaOrder.create(beta_order_params)
 
-        beta_order = BetaOrder.create(beta_order_params.merge(delivery_time: start_time))
-
-        # send a booking request to TimeKit
-        event = {
-          start: start_time.to_time.iso8601,
-          end: end_time.to_time.iso8601,
-          what: 'Tzukuri Personal Fitting - ' + current_beta_user.name,
-          where: beta_order.full_address,
-          calendar_id: '348c52b6-ae68-40d3-9781-2ea308505f04',
-          description: ''
-        }
-
-        customer = {
-          name: current_beta_user.name,
-          email: current_beta_user.email,
-          phone: beta_order.phone,
-          voip: '',
-          # fixme: assuming that everyone is in Sydney for now
-          timezone: 'Australia/Sydney'
-        }
-
-        # don't notify the customer by email, we'll send our own confirmation
-        notify = {
-          enabled: false
-        }
-
-        # create a new timekit instance, all bookings all go into the beta@tzukuri.com calendar
-        # eventually, we'll probably want to change this and have a seperate email for handling bookings
-        timekit = Tzukuri::Timekit.new("beta@tzukuri.com", 'XAuyu7wMLLjSlF6pltBJR4x8d4W3tN7W')
-
-        begin
-          response = timekit.create_booking(event, customer, notify)
-
-          # update the beta order with the Timekit booking id
-          booking = JSON.parse(response.body)
-          beta_order.update_attribute('booking_id', booking["data"]["id"])
-        rescue => e
-          # if there was an error creating the booking, destroy the order and return an error
-          beta_order.destroy
-          render :json => {success: false, errors: ["An error ocurred creating your booking. Please try again."]}
-          return
-        end
-      else
-        beta_order = BetaOrder.create(beta_order_params)
-      end
-
-      if !beta_order.valid?
-        render :json => {success: false, errors: beta_order.errors}
+    # if we have start and end times, try to create a booking
+    if params[:timeslot_start].present? && params[:timeslot_end].present?
+      begin
+        beta_order.create_delivery_booking(params[:timeslot_start], params[:timeslot_end])
+      rescue => e
+        beta_order.destroy
+        render :json => {success: false, errors: ["An error ocurred creating your booking. Please try again."]}
         return
       end
-
-      beta_order.send_confirmation_email
-      render :json => {success: true, beta_order: beta_order}
     end
+
+    if !beta_order.valid?
+      render :json => {success: false, errors: beta_order.errors}
+      return
+    end
+
+    beta_order.send_confirmation_email
+    render :json => {success: true, beta_order: beta_order}
   end
 
   private
--- app/models/beta/beta_order.rb
+++ app/models/beta/beta_order.rb
@@ -22,6 +22,50 @@ class BetaOrder < ActiveRecord::Base
     BetaMailer.send_beta_order_email(self.beta_user).deliver_later
   end
 
+  def valid_delivery?
+    delivery_time.present? && booking_id.present? && delivery_method == "deliver"
+  end
+
+  # given a start and end time, try to create a booking with timekit
+  # this will override any current booking on this object
+  # todo: let this delete any existing booking in Timekit (currently unsupported)
+  def create_delivery_booking(start_time, end_time)
+      event = {
+        start: start_time.to_time.iso8601,
+        end: end_time.to_time.iso8601,
+        what: 'Tzukuri Personal Fitting - ' + beta_user.name,
+        where: full_address,
+        calendar_id: '348c52b6-ae68-40d3-9781-2ea308505f04',
+        description: ''
+      }
+
+      customer = {
+        name: beta_user.name,
+        email: beta_user.email,
+        phone: phone,
+        voip: '',
+        # fixme: assuming that everyone is in Sydney for now
+        timezone: 'Australia/Sydney'
+      }
+
+      # don't notify the customer by email, we'll send our own confirmation
+      notify = {
+        enabled: false
+      }
+
+      timekit = Tzukuri::Timekit.new("beta@tzukuri.com", 'XAuyu7wMLLjSlF6pltBJR4x8d4W3tN7W')
+
+      begin
+        response = timekit.create_booking(event, customer, notify)
+        booking = JSON.parse(response.body)
+        # set the delivery time and the booking ID now that booking is confirmed
+        update_attribute('delivery_time', start_time)
+        update_attribute('booking_id', booking["data"]["id"])
+      rescue => e
+        # todo: handle an error
+      end
+  end
+
   def self.all_next_week
     BetaOrder.order('delivery_time asc').where('delivery_time BETWEEN ? AND ?', Date.today.beginning_of_day, (Date.today + 7.days).end_of_day)
   end
