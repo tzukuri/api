bf7cf94543e8c886e46c7b68be9cfd63fdb1bb43
Sam Turner <samturner.23@me.com>
2016-12-19 19:27:57 +1100

    update config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require 'skippable_uglifier'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   # prevent partial rendering in log
   config.action_view.logger = nil
 
@@ -34,7 +34,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
