2c02786a1b0dd906b8487c2f41e66df32f6cc0ec
Sam Turner <samturner.23@me.com>
2016-12-19 20:26:04 +1100

    Added core.js

---
+++ app/assets/javascripts/core.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.serializeObject.min
+//= require lodash.min
+//= require stickyfill.min
+//= require moment
+//= require_tree ./tzukuri/
