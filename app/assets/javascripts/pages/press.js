93320aa1962d25b9ef65ff223009796e40bbd0e0
Will Cannings <me@willcannings.com>
2017-03-10 13:04:31 +1100

    Better article expansion and link to press in footer

--- app/assets/javascripts/pages/press.js
+++ app/assets/javascripts/pages/press.js
@@ -1,10 +1,17 @@
 $(function() {
     $('.continue a').click(function(e) {
-        e.preventDefault();
         var link = $(this);
-        var article = link.closest('.article');
+        var parent = link.closest('.continue');
+        var article = parent.closest('.article');
         var section = article.find('section.bottom');
+
+        // prevent scrolling to the top of the page
+        e.preventDefault();
+
+        // show the 'bottom' (expanded) content
         section.show();
-        link.hide();
+
+        // hide the p element containing the link
+        parent.hide();
     });
 });
