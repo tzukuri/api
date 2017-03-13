212dada9350cf844b724bc356a6390e177bd6f39
Will Cannings <me@willcannings.com>
2017-03-13 12:19:14 +1100

    Links to specific press articles on index, with corrected scrolling behaviour

--- app/assets/javascripts/pages/press.js
+++ app/assets/javascripts/pages/press.js
@@ -1,3 +1,25 @@
+// when onLoad event fires (all resources ready and page is (or should be) laid out)
+$(window).load(function() {
+    // if the user clicked through to a specific article, scroll to the article,
+    // but offset for the header (the browser will scroll the top of the article
+    // underneath the header i.e at 0px otherwise)
+    var article = $(window.location.hash);
+    if (article.length != 0) {
+        var offset = article.offset();
+        if (offset && offset.top) {
+            var scroll = offset.top;
+            scroll -= 85; // header: 65px, + padding: 20px
+
+            // delay scrolling so we can override the browser's own scrolling behaviour
+            // (it will scroll the element but won't offset the header)
+            setTimeout(function() {
+                $(window).scrollTop(scroll);
+            }, 100);
+        }
+    }
+});
+
+
 $(function() {
     $('.continue a').click(function(e) {
         var link = $(this);
