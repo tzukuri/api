2466c3bf96b501f2cb2ef48b73669496d0d59729
Will Cannings <me@willcannings.com>
2017-03-14 15:47:26 +1100

    Included network, username, comment, and source urls for all images in the gallery - will show a popover of the comment and link to the social network eventually

--- app/assets/javascripts/pages/gallery.js
+++ app/assets/javascripts/pages/gallery.js
@@ -1,1 +1,19 @@
 //= require jquery.justified
+
+$(function() {
+    $('#images-container').empty().justifiedImages({
+        images : photos,
+        rowHeight: 400,
+        maxRowHeight: 500,
+
+        thumbnailPath: function(photo, width, height){
+            return photo.url;
+        },
+
+        getSize: function(photo) {
+            return {width: photo.width, height: photo.height};
+        },
+
+        margin: 1
+    });
+});
