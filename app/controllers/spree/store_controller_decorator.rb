5c35ce7c215dfcfb7de0ddac5fa513b619d77586
Sam Turner <samturner.23@me.com>
2017-05-24 19:34:19 +1000

    Cleaning up. Better images on home page, fix issues with tables on mobile, fix email signup form on mobile, update taxon views for mobile, using correct image sizes on checkout

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::StoreController.class_eval do
   def inject_no_bg_header
-    no_bg_pages = ['spree/taxons', 'spree/products', 'spree/orders', 'spree/checkout']
+    no_bg_pages = ['spree/products', 'spree/orders', 'spree/checkout']
     return 'no-bg' if no_bg_pages.include? params[:controller]
   end
 
